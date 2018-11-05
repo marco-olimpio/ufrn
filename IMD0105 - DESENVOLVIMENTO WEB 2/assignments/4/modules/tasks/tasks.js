193b5b15e6a8274f5ba44cbf863add0527c3d0cd
marco_olimpio <marco.olimpio@gmail.com>
2018-11-04 21:33:14 -0300

    feat(IMD105): assignment 4 - login/logout

---
+++ IMD0105 - DESENVOLVIMENTO WEB 2/assignments/4/modules/tasks/tasks.js
@@ -0,0 +1,85 @@
+/**
+ * This module is responsible for control tasks
+ * @author Marco Olimpio - marco.olimpio at gmail.com
+ * @ver 0.1
+ */
+
+var fs = require('fs');
+var taskdb = "./db/tasks.json";
+
+/**
+ * Add tasks to the database
+ * @id The task ID
+ * @descricao The description of the task
+ * @detalhe The detailed description of the task
+ * @data The deadline date
+ * @taskStatus One of the 'todo', 'doing' or 'done' status
+ * @return true if the operation with success
+ */
+function addtsk(id, descricao, detalhe, data, taskStatus){
+	var db = loadDb();
+	try{
+		db.task.push({"id":id, "descricao": descricao, "detalhe": detalhe,"data": data,"status": taskStatus});	
+		return true;
+	}catch(err)
+		console.log(err);
+	return false;	
+}
+
+/**
+ * */
+function updatetsk(id, descricao, detalhe, data, taskStatus){
+		try{
+			var task = findTaskById(id);
+			task.['descricao'] = descricao;
+			task.['detalhe'] = detalhe;
+			task.['data'] = data;
+			task.['status'] = taskStatus;
+
+			return true;	
+		}catch(err)
+			console.log(err);
+		return false;
+}
+
+/**
+ * */
+function deletetsk(id){
+		var db = loadDb();
+		try{
+			delete db.task.id;
+			return true;
+		}catch(err)
+				console.log(err);
+		return false;
+}
+
+function readtsk(id){
+	return findTaskById(id);	
+}
+
+function readalltsk(){
+	var db = loadDb();
+		
+	return db;	
+}
+
+function loadDb(){
+		var content = fs.readFileSync(taskdb);
+		return dbContent = JSON.parse(content);
+}
+
+function findTaskById(id){
+	var db = loadDb();
+	var i;
+	for(i=0; i< db.task.length; i++){
+		if(id == db.task[i].id)
+			return db.task[i];
+	}
+
+module.exports.addtsk = addtsk;
+module.exports.updatetsk = updatetsk;
+module.exports.deletetsk = deletetsk;
+module.exports.readtsk = readtsk;
+module.exports.readalltsk = readalltsk;
+
